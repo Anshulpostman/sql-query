1bfff3d91992e42d58cd261d5d80c3a4e9ebb9a4
Anshul <anshul.a.iper@gmail.com>
2024-11-16 11:43:53 +0530

    addquery

--- innerjoin.sql
+++ innerjoin.sql
@@ -4,7 +4,19 @@
 
 --Inner Join Pratice
 
-SELECT Students.Studentid, Teachers.Hiredate, Students.LastName 
-FROM Students 
-INNER JOIN Teachers 
-ON Students.FirstName = Teachers.FirstName AND Students.LastName = Teachers.LastName;
+--SELECT Students.Studentid, Teachers.Hiredate, Students.LastName 
+--FROM Students 
+--INNER JOIN Teachers 
+--ON Students.FirstName = Teachers.FirstName AND Students.LastName = Teachers.LastName;
+
+
+--SELECT Students.Studentid, Teachers.Hiredate, Students.LastName 
+--FROM Students 
+ --JOIN Teachers 
+--ON Students.FirstName = Teachers.FirstName AND Students.LastName = Teachers.LastName;
+
+Select purchasing.PurchaseOrderLines.PurchaseOrderlineid, purchasing.PurchaseOrders.SupplierID , purchasing.PurchaseOrders.Orderdate,purchasing.SupplierCategories.SupplierCategoryID
+from purchasing.PurchaseOrderLines 
+INNER JOIN purchasing.PurchaseOrders on PurchaseOrderLines.PurchaseorderID = PurchaseOrders.PurchaseorderID 
+INNER JOIN purchasing.SupplierCategories on purchasing.PurchaseOrders.SupplierID = purchasing.SupplierCategories.SupplierCategoryID
+---- Three table inner join
