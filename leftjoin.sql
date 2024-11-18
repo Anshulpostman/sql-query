907180c2d86aaaa1ae54b227ccc706b87bb59e5f
Anshul <anshul.a.iper@gmail.com>
2024-11-18 21:04:05 +0530

    lrjoin

---
+++ leftjoin.sql
@@ -0,0 +1,4 @@
+--- Left jon
+
+--select firstname , LastName from Students left join Enrollments on Students.Studentid = Enrollments.Studentid 
+select coursename from Courses left join Enrollments on Courses.Courseid=Enrollments.Enrollmentid
