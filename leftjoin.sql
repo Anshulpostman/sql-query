5751c89d6df8f63f5630fb986a7f1f7b4d73a855
Anshulpostman <114227437+Anshulpostman@users.noreply.github.com>
2024-11-18 21:06:01 +0530

    Update leftjoin.sql

--- leftjoin.sql
+++ leftjoin.sql
@@ -2,3 +2,12 @@
 
 --select firstname , LastName from Students left join Enrollments on Students.Studentid = Enrollments.Studentid 
 select coursename from Courses left join Enrollments on Courses.Courseid=Enrollments.Enrollmentid
+select coursename from Courses left join Enrollments on Courses.Courseid=Enrollments.Enrollmentid
+select coursename from Courses left join Enrollments on Courses.Courseid=Enrollments.Enrollmentid
+select coursename from Courses left join Enrollments on Courses.Courseid=Enrollments.Enrollmentid
+select coursename from Courses left join Enrollments on Courses.Courseid=Enrollments.Enrollmentid
+select coursename from Courses left join Enrollments on Courses.Courseid=Enrollments.Enrollmentid
+select coursename from Courses left join Enrollments on Courses.Courseid=Enrollments.Enrollmentid
+select coursename from Courses left join Enrollments on Courses.Courseid=Enrollments.Enrollmentid
+v
+v
