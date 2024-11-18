40f5f09526477cbf5c5393788f688aec12e1a491
Anshul <anshul.a.iper@gmail.com>
2024-11-18 20:52:43 +0530

    Initial commit

--- joins.sql
+++
@@ -1,9 +0,0 @@
---left join
-select Customers.CustomerID,Customers.CompanyName, Customers.CompanyName,Orders.OrderID, Orders.OrderDate from Customers left join Orders
-on Customers.CustomerID=Orders.ShipAddress
-
-
----right join
-
-select Customers.CustomerID,Customers.CompanyName, Customers.CompanyName,Orders.OrderID, Orders.OrderDate from Customers right join Orders
-on Customers.CustomerID=Orders.ShipAddress
