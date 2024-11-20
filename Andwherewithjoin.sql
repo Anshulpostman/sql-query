ca309e23596b04e204b6169219dd188dbc2106bb
Anshul <anshul.a.iper@gmail.com>
2024-11-20 10:23:48 +0530

    updatefullouterjoin

--- Andwherewithjoin.sql
+++ Andwherewithjoin.sql
@@ -13,5 +13,9 @@ Customers on Orders.CustomerID= Customers.CustomerID  where ShipName='Vins et al
 select Orders.OrderID, Orders.OrderDate, orders.ShipName,Customers.CompanyName,Customers.ContactName, Customers.Country from Orders left join
 Customers on Orders.CustomerID= Customers.CustomerID  where ShipName='Vins et alcools Chevalier'
 
-select Orders.OrderID, Orders.OrderDate, orders.ShipName,Customers.CompanyName,Customers.ContactName, Customers.Country from Orders left join
+select Orders.OrderID, Orders.OrderDate, orders.ShipName,Customers.CompanyName,Customers.ContactName, Customers.Country from Orders full join
 Customers on Orders.CustomerID= Customers.CustomerID  where ShipName='Toms Spezialitäten'
+
+--full outer join
+select Customers.CompanyName, Customers.ContactName, orders.ShipName,orders.ShipAddress from Customers 
+FULL OUTER JOIN Orders on Customers.CustomerID=orders.CustomerID
--- innerjoin.sql
+++ innerjoin.sql
@@ -15,8 +15,8 @@
  --JOIN Teachers 
 --ON Students.FirstName = Teachers.FirstName AND Students.LastName = Teachers.LastName;
 
-Select purchasing.PurchaseOrderLines.PurchaseOrderlineid, purchasing.PurchaseOrders.SupplierID , purchasing.PurchaseOrders.Orderdate,purchasing.SupplierCategories.SupplierCategoryID
-from purchasing.PurchaseOrderLines 
-INNER JOIN purchasing.PurchaseOrders on PurchaseOrderLines.PurchaseorderID = PurchaseOrders.PurchaseorderID 
-INNER JOIN purchasing.SupplierCategories on purchasing.PurchaseOrders.SupplierID = purchasing.SupplierCategories.SupplierCategoryID
+--Select purchasing.PurchaseOrderLines.PurchaseOrderlineid, purchasing.PurchaseOrders.SupplierID , purchasing.PurchaseOrders.Orderdate,purchasing.SupplierCategories.SupplierCategoryID
+--from purchasing.PurchaseOrderLines 
+--INNER JOIN purchasing.PurchaseOrders on PurchaseOrderLines.PurchaseorderID = PurchaseOrders.PurchaseorderID 
+--INNER JOIN purchasing.SupplierCategories on purchasing.PurchaseOrders.SupplierID = purchasing.SupplierCategories.SupplierCategoryID
 ---- Three table inner join
